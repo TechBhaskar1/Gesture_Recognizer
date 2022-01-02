2b22f1976412d0c03e2178e112dca127bdd65d49
TechBhaskar1 <81464944+TechBhaskar1@users.noreply.github.com>
2022-01-02 13:29:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //https://teachablemachine.withgoogle.com/models/ldtatEyPc/
-prediction_1 = ''
-prediction_2 = ''
+prediction_1 = '';
+prediction_2 = '';
+
 Webcam.set({
     width: 350,
     height: 300,
@@ -8,28 +9,81 @@ Webcam.set({
     png_quality: 90
 });
 
-camera = document.getElementById("camera")
+camera = document.getElementById("camera");
 
-Webcam.attach('#camera')
+Webcam.attach('#camera');
 
 function capture() {
     Webcam.snap(function (data_uri) {
-        document.getElementById("result").innerHTML = "<img id='captured_image' src='" + data_uri + "'/> "
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='" + data_uri + "'/> ";
     });
 }
 
-console.log("ml5 version :" + ml5.version)
+console.log("ml5 version :" + ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ldtatEyPc/model.json", modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ldtatEyPc/model.json', modelLoaded);
 
 function modelLoaded() {
-    console.log(modelLoaded)
+    console.log("modelLoaded");
 }
 
 function speak() {
-    var synK = window.speechSynthesis;
+    var sync = window.speechSynthesis;
     speak_data1 = "First Prediction is " + prediction_1;
     speak_data2 = "Second Prediction is " + prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
-    sync.speak(utterThis); 
+    sync.speak(utterThis);
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } 
+    else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+
+        if (results[0].label == "Awesome") {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if (results[0].label == "Good") {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if (results[0].label == "Victory") {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if (results[0].label == "Yoo!") {
+            document.getElementById("update_emoji").innerHTML = "&#129304;";
+        }
+        if (results[0].label == "Namaste") {
+            document.getElementById("update_emoji").innerHTML = "&#128591;";
+        }
+
+        if (results[1].label == "Awesome") {
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        }
+        if (results[1].label == "Good") {
+            document.getElementById("update_emoj2").innerHTML = "&#128077;";
+        }
+        if (results[1].label == "Victory") {
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        }
+        if (results[1].label == "Yoo!") {
+            document.getElementById("update_emoji2").innerHTML = "&#129304;";
+        }
+        if (results[1].label == "Namaste") {
+            document.getElementById("update_emoji2").innerHTML = "&#128591;";
+        }
+
+
+    }
 }
